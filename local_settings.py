5005d50fbe1ec927fce5a86e1bd45bdc1daedfa3
root <root@VM-178-22-ubuntu.(none)>
2014-11-10 19:48:25 +0800

    能用的cms。包括 oweb.nginx.conf 和静态文件

--- local_settings.py
+++ local_settings.py
@@ -1,5 +1,5 @@
 
-DEBUG = True
+DEBUG = False
 
 # Make these unique, and don't share it with anybody.
 SECRET_KEY = "cdcee9db-0af6-487a-85b6-5aeeffa2ca8eb80f5d1f-5637-49fe-97fc-7abb050083f55178c939-b8fa-4983-a2bf-8636c737ab44"
@@ -7,17 +7,17 @@ NEVERCACHE_KEY = "15c7b70b-9d57-4861-b6c6-8c27b16c650ac345b268-8737-49b0-97ac-52
 
 DATABASES = {
     "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.sqlite3",
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.mysql",
         # DB name or path to database file if using sqlite3.
-        "NAME": "dev.db",
+        "NAME": "oweb",
         # Not used with sqlite3.
-        "USER": "",
+        "USER": "root",
         # Not used with sqlite3.
-        "PASSWORD": "",
+        "PASSWORD": "openrpi2014",
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
+        "HOST": "10.66.110.161",
         # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
+        "PORT": "3306",
     }
 }

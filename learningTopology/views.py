b9eae5e7c6bb1b6ad2c25591cd245f1e213e6582
izyh <478290135@qq.com>
2015-05-12 09:18:25 +0800

    debug2

--- learningTopology/views.py
+++ learningTopology/views.py
@@ -16,7 +16,7 @@ from mezzanine.utils.models import get_user_model
 from learningTopology.models import Path, Node
 
 def learn_path_detail(request, template="learn/path_detail.html"):
-	return HttpResponse("HelloWorld!")
+	return HttpResponse("ok")
 	s = request.GET['s']
 	e = request.GET['e']
 	try:
